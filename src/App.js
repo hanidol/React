c6307aa27ec49b0d527212f06fb22c0da3dd0531
hanidol <haninasri14@gmail.com>
2022-08-30 09:28:51 +0100

    first Commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import logo from "./apple.svg";
+import "./App.css";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+function App() {
+  return (
+    <Container>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Go <code>Share Your Works </code> With The World.
+          </p>
+          <a
+            className="App-link"
+            href="https://www.apple.com/fr/mac/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Go To Apple Site
+          </a>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </header>
+        <Row className="justify-content-md-center">
+          <Col md="auto">By Hani Nasri</Col>
+        </Row>
+      </div>
+    </Container>
+  );
+}
+
+export default App;
